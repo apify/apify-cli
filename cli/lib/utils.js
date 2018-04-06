fc355e230c5caae2bde13245131773a121a106ca
JakubDrobnik <drobnik.j@gmail.com>
2018-04-06 09:33:17 +0200

    fix #3, add test for utils

--- cli/lib/utils.js
+++ cli/lib/utils.js
@@ -108,17 +108,20 @@ const argsToCamelCase = (object) => {
 };
 
 /**
- * Create zip files with all act files, omit files regarding .gitignore settings
+ * Create zip files with all act files in current directory, omit files regarding .gitignore settings and ignore .git folder.
+ * NOTE: Zips .file files and .folder/ folders
  * @param zipName
  * @return {Promise<void>}
  */
 const createActZip = async (zipName) => {
-    const excludedPaths = gitignore('.gitignore').map(patern => `!${patern}`);
-    const pathsToZip = await globby(['*', '*/**', ...excludedPaths]);
+    const pathsToZip = await globby(['*', '**/**'], { ignore: ['.git/**'], gitignore: true, dot: true });
+
     const archive = archiver(zipName);
+
     const archiveFilesPromises = [];
     pathsToZip.forEach(globPath => archiveFilesPromises.push(archive.glob(globPath)));
     await Promise.all(archiveFilesPromises);
+
     await archive.finalize();
 };
 
