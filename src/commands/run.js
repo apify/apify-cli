c718146e168fcb3759c3c86c7ac572b816d8381a
JakubDrobnik <drobnik.j@gmail.com>
2018-04-06 16:52:25 +0200

    Merge commands/run.js, which is not merge before

--- src/commands/run.js
+++ src/commands/run.js
@@ -3,6 +3,8 @@ const execWithLog = require('../lib/exec');
 const { LOCAL_ENV_VARS, PROXY_PASSWORD_ENV_VAR } = require('../lib/consts');
 const { getLocalUserInfo } = require('../lib/utils');
 
+// TODO: Shall we also pass APIFY_TOKEN ?
+
 class RunCommand extends ApifyCommand {
     static async run() {
         const { proxy } = getLocalUserInfo();
@@ -15,8 +17,12 @@ class RunCommand extends ApifyCommand {
     }
 }
 
-RunCommand.description = `
-This runs act locally from current directory. It uses apify_local for getting input and setting output and storing data.
-`;
+// TODO: we should describe which env vars are set here:
+
+RunCommand.description = 'Runs the act locally in the current directory.\n' +
+    'The command runs a Node.js process with the act in the current directory. It sets various APIFY_XYZ environment variables' +
+    'in order to provide a working execution environment for the act. For example, this causes ' +
+    'the act input, as well as all other data in key-value stores, datasets or request queues to be stored in the "apify_local" directory,' +
+    'rather than on the Apify platform.';
 
 module.exports = RunCommand;
