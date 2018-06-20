14d42b7bf5ba9a02068f0c3cb5f9820a951cc740
JakubDrobnik <drobnik.j@gmail.com>
2018-06-20 18:50:53 +0200

    Add options to purge stores to apify run command

--- src/commands/run.js
+++ src/commands/run.js
@@ -1,22 +1,48 @@
 const { ApifyCommand } = require('../lib/apify_command');
+const { flags: flagsHelper } = require('@oclif/command');
+const fs = require('fs');
+const path = require('path');
 const execWithLog = require('../lib/exec');
-const { LOCAL_ENV_VARS } = require('../lib/consts');
+const { LOCAL_ENV_VARS, MAIN_FILE } = require('../lib/consts');
 const { ENV_VARS } = require('apify-shared/consts');
-const { getLocalUserInfo } = require('../lib/utils');
+const { getLocalUserInfo, purgeDefaultQueue, purgeDefaultKeyValueStore, purgeDefaultDataset } = require('../lib/utils');
+const { info } = require('../lib/outputs');
 
 class RunCommand extends ApifyCommand {
-    static async run() {
+    async run() {
+        const { flags } = this.parse(RunCommand);
         const { proxy, id: userId, token } = getLocalUserInfo();
         const apifyLocalEnvVars = LOCAL_ENV_VARS;
+        const cwd = process.cwd();
+
+        const mainJsFile = path.join(cwd, MAIN_FILE);
+        if (!fs.existsSync(mainJsFile)) {
+            throw new Error('File main.js is missing in current dir! Call "apify init" to create it.');
+        }
 
         if (proxy && proxy.password) apifyLocalEnvVars[ENV_VARS.PROXY_PASSWORD] = proxy.password;
         if (userId) apifyLocalEnvVars[ENV_VARS.USER_ID] = userId;
         if (token) apifyLocalEnvVars[ENV_VARS.TOKEN] = token;
 
+        // Purge stores
+        if (flags.purgeStores) {
+            await Promise.all([purgeDefaultQueue(cwd), purgeDefaultKeyValueStore(cwd), purgeDefaultDataset(cwd)]);
+            info('All default local stores were purge.');
+        } else if (flags.purgeQueue) {
+            await purgeDefaultQueue(cwd);
+            info('Default page queue was purge.');
+        } else if (flags.purgeDataset) {
+            await purgeDefaultDataset(cwd);
+            info('Default page dataset was purge.');
+        } else if (flags.purgeKeyValueStore) {
+            await purgeDefaultKeyValueStore(cwd);
+            info('Default key-value store was purge.');
+        }
+
         // NOTE: User can overwrite env vars
         const env = Object.assign(apifyLocalEnvVars, process.env);
 
-        await execWithLog('node', ['main.js'], { env });
+        await execWithLog('node', [MAIN_FILE], { env });
     }
 }
 
@@ -27,5 +53,28 @@ RunCommand.description = 'Runs the act locally in the current directory.\n' +
     'in order to provide a working execution environment for the act. For example, this causes ' +
     'the act input, as well as all other data in key-value stores, datasets or request queues to be stored in the "apify_local" directory, ' +
     'rather than on the Apify platform.';
+
+RunCommand.flags = {
+    'purge-stores': flagsHelper.boolean({
+        char: 'ps',
+        description: '',
+        required: false,
+    }),
+    'purge-queue': flagsHelper.boolean({
+        char: 'bq',
+        description: '',
+        required: false,
+    }),
+    'purge-dataset': flagsHelper.boolean({
+        char: 'pd',
+        description: '',
+        required: false,
+    }),
+    'purge-key-value-store': flagsHelper.boolean({
+        char: 'pk',
+        description: '',
+        required: false,
+    }),
+};
 
 module.exports = RunCommand;
--- src/lib/files.js
+++ src/lib/files.js
@@ -2,6 +2,7 @@ const loadJson = require('load-json-file');
 const writeJson = require('write-json-file');
 const fs = require('fs');
 const rimraf = require('rimraf');
+const { promisify } = require('util');
 
 const updateLocalJson = async (path, updateAttrs = {}, nestedObjectAttr) => {
     const currentObject = await loadJson(path);
@@ -32,4 +33,14 @@ const rimrafPromised = (path) => {
     });
 };
 
-module.exports = { updateLocalJson, createFolderSync, rimrafPromised };
+const fileStat = promisify(fs.stat);
+const unlinkFile = promisify(fs.unlink);
+
+const deleteFile = async (path) => {
+    const stat = await fileStat(path);
+    if (stat.isFile()) {
+        await unlinkFile(path);
+    }
+};
+
+module.exports = { updateLocalJson, createFolderSync, rimrafPromised, deleteFile };
