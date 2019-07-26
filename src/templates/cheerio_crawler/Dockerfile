7c889fd9db35e63738ebb0de197d7ed5da425f7d
Glenn <glenn@apify.com>
2019-07-26 12:17:43 +0200

    Import correct cheerio example and change docker image

--- src/templates/cheerio_crawler/Dockerfile
+++ src/templates/cheerio_crawler/Dockerfile
@@ -9,7 +9,7 @@
 #  apify/actor-node-chrome-xvfb (Node.js 10 + Chrome + Xvfb on Debian)
 # For more information, see https://apify.com/docs/actor#base-images
 # Note that you can use any other image from Docker Hub.
-FROM apify/actor-node-chrome
+FROM apify/actor-node-basic
 
 # Second, copy just package.json and package-lock.json since they are the only files
 # that affect NPM install in the next step
--- src/templates/cheerio_crawler/main.js
+++ src/templates/cheerio_crawler/main.js
@@ -1,41 +1,70 @@
 const Apify = require('apify');
-const requestPromise = require('request-promise');
+
+// Apify.utils contains various utilities, e.g. for logging.
+// Here we turn off the logging of unimportant messages.
+const { log } = Apify.utils;
+log.setLevel(log.LEVELS.WARNING);
+
+// A link to a list of Fortune 500 companies' websites available on GitHub.
+const CSV_LINK = 'https://gist.githubusercontent.com/hrbrmstr/ae574201af3de035c684/raw/f1000.csv';
 
 // Apify.main() function wraps the crawler logic (it is optional).
 Apify.main(async () => {
-    // Create and initialize an instance of the RequestList class that contains
-    // a list of URLs to crawl. Here we use just a few hard-coded URLs.
+    // Create an instance of the RequestList class that contains a list of URLs to crawl.
+    // Here we download and parse the list of URLs from an external file.
     const requestList = new Apify.RequestList({
-        sources: [
-            { url: 'http://www.google.com/' },
-            { url: 'http://www.example.com/' },
-            { url: 'http://www.bing.com/' },
-            { url: 'http://www.wikipedia.com/' },
-        ],
+        sources: [{ requestsFromUrl: CSV_LINK }],
     });
     await requestList.initialize();
 
-    // Create a BasicCrawler - the simplest crawler that enables
-    // users to implement the crawling logic themselves.
-    const crawler = new Apify.BasicCrawler({
-
+    // Create an instance of the CheerioCrawler class - a crawler
+    // that automatically loads the URLs and parses their HTML using the cheerio library.
+    const crawler = new Apify.CheerioCrawler({
         // Let the crawler fetch URLs from our list.
         requestList,
 
+        // The crawler downloads and processes the web pages in parallel, with a concurrency
+        // automatically managed based on the available system memory and CPU (see AutoscaledPool class).
+        // Here we define some hard limits for the concurrency.
+        minConcurrency: 10,
+        maxConcurrency: 50,
+
+        // On error, retry each page at most once.
+        maxRequestRetries: 1,
+
+        // Increase the timeout for processing of each page.
+        handlePageTimeoutSecs: 60,
+
         // This function will be called for each URL to crawl.
-        // The 'request' option is an instance of the Request class, which contains
-        // information such as URL and HTTP method, as supplied by the RequestList.
-        handleRequestFunction: async ({ request }) => {
+        // It accepts a single parameter, which is an object with the following fields:
+        // - request: an instance of the Request class with information such as URL and HTTP method
+        // - html: contains raw HTML of the page
+        // - $: the cheerio object containing parsed HTML
+        handlePageFunction: async ({ request, html, $ }) => {
             console.log(`Processing ${request.url}...`);
 
-            // Fetch the page HTML
-            const html = await requestPromise(request.url);
+            // Extract data from the page using cheerio.
+            const title = $('title').text();
+            const h1texts = [];
+            $('h1').each((index, el) => {
+                h1texts.push({
+                    text: $(el).text(),
+                });
+            });
 
-            // Store the HTML and URL to the default dataset.
+            // Store the results to the default dataset. In local configuration,
+            // the data will be stored as JSON files in ./apify_storage/datasets/default
             await Apify.pushData({
                 url: request.url,
+                title,
+                h1texts,
                 html,
             });
+        },
+
+        // This function is called if the page processing failed more than maxRequestRetries+1 times.
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed twice.`);
         },
     });
 
