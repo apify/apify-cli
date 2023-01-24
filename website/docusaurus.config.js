a28d14db44fc92f45cdee17ca4de11a118994d3b
Jindřich Bär <jindrichbar@gmail.com>
2023-01-24 10:04:15 +0100

    docs: activeBaseRegex fix for Docs

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -29,7 +29,7 @@ module.exports = {
                             to: 'docs',
                             label: 'Docs',
                             position: 'left',
-                            activeBaseRegex: 'docs/(?!changelog)',
+                            activeBaseRegex: 'docs(?!/changelog)',
                         },
                         {
                             to: 'docs/changelog',
