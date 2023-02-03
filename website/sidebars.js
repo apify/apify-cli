b3402b8495128dab75b626b667b5eeb667823559
Jindřich Bär <jindrichbar@gmail.com>
2023-02-03 18:17:36 +0100

    docs: update subnavbar, unnest the docs categories

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,30 +1,24 @@
 module.exports = [
     [
         {
-            type: 'category',
-            label: 'Using Apify CLI',
-            items: [
-                {
-                    type: 'doc',
-                    id: 'index',
-                    label: 'Apify CLI',
-                },
-                {
-                    type: 'doc',
-                    id: 'installation',
-                    label: 'Installation',
-                },
-                {
-                    type: 'doc',
-                    id: 'vars',
-                    label: 'Environment variables',
-                },
-                {
-                    type: 'doc',
-                    id: 'commands',
-                    label: 'Command reference',
-                },
-            ],
+            type: 'doc',
+            id: 'index',
+            label: 'Apify CLI',
+        },
+        {
+            type: 'doc',
+            id: 'installation',
+            label: 'Installation',
+        },
+        {
+            type: 'doc',
+            id: 'vars',
+            label: 'Environment variables',
+        },
+        {
+            type: 'doc',
+            id: 'reference',
+            label: 'Command reference',
         },
         {
             type: 'doc',
