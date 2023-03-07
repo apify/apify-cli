7d810f6a8cf13d4bf01286b7ccbc6cf2d2e147a0
František Nesveda <fnesveda@users.noreply.github.com>
2023-03-07 12:02:00 +0100

    feat: Print correct update command based on package manager (#342)
    In preparation for publishing the Apify CLI on Homebrew, this adds
    detection through which package manager was the CLI installed, and
    adjusts the update message to be correct for the given package manager.

--- src/lib/version_check.js
+++ src/lib/version_check.js
@@ -1,3 +1,5 @@
+const { execSync } = require('child_process');
+const process = require('process');
 const axios = require('axios');
 const chalk = require('chalk');
 const semver = require('semver');
@@ -13,17 +15,43 @@ const {
     extendLocalState,
 } = require('./local_state');
 
+const INSTALLATION_TYPE = {
+    HOMEBREW: 'HOMEBREW',
+    NPM: 'NPM',
+};
+
+const UPDATE_COMMAND = {
+    [INSTALLATION_TYPE.HOMEBREW]: 'brew update && brew upgrade apify-cli',
+    [INSTALLATION_TYPE.NPM]: 'npm install -g apify-cli@latest',
+};
+
+/**
+ * Detect through which package manager the Apify CLI was installed.
+ * @returns {INSTALLATION_TYPE} The installation type of the CLI.
+ */
+const detectInstallationType = () => {
+    // The path of the alias to the `src/bin/run` file is in process.argv[1]
+    const commandPath = process.argv[1];
+
+    if (commandPath) {
+        // If the real command path is like `/opt/homebrew/Cellar/apify-cli/...` or `/home/linuxbrew/.linuxbrew/Cellar/apify-cli/...`,
+        // then the CLI is installed via Homebrew
+        if (process.platform === 'linux' || process.platform === 'darwin') {
+            const realCommandPath = execSync(`realpath "${commandPath}"`);
+            if (realCommandPath.includes('homebrew/Cellar') || realCommandPath.includes('linuxbrew/Cellar')) {
+                return INSTALLATION_TYPE.HOMEBREW;
+            }
+        }
+        // Add more install types here once we have the CLI in other package managers
+    }
+
+    // If we didn't detect otherwise, assume the CLI was installed through NPM
+    return INSTALLATION_TYPE.NPM;
+};
+
 const getLatestNpmVersion = async () => {
-    const response = await axios({
-        url: 'https://registry.npmjs.org/apify-cli/',
-        headers: {
-            // This is necessary so that NPM returns the abbreviated version of the metadata
-            // See https://github.com/npm/registry/blob/master/docs/responses/package-metadata.md
-            accept: 'application/vnd.npm.install-v1+json',
-        },
-    });
-    const packageMetadata = response.data;
-    const latestVersion = packageMetadata['dist-tags'].latest;
+    const response = await axios({ url: 'https://registry.npmjs.org/apify-cli/latest' });
+    const latestVersion = response.data.version;
     return latestVersion;
 };
 
@@ -72,9 +100,11 @@ const checkLatestVersion = async (enforeUpdate = false) => {
     const currentNpmVersion = require('../../package.json').version; //  eslint-disable-line
 
     if (latestNpmVersion && semver.gt(latestNpmVersion, currentNpmVersion)) {
+        const installationType = detectInstallationType();
+        const updateCommand = `' ${UPDATE_COMMAND[installationType]} '`;
         console.log('');
         warning('You are using an old version of Apify CLI. We strongly recommend you always use the latest available version.');
-        console.log(`       ↪ Run ${chalk.bgWhite(chalk.black(' npm install apify-cli@latest -g '))} to install it! 👍 \n`);
+        console.log(`       ↪ Run ${chalk.bgWhite(chalk.black(updateCommand))} to update! 👍 \n`);
     } else if (shouldGetCurrentVersion) {
         // In this case the version was refreshed from the NPM which took a while and "Info: Making sure that Apify ..." was printed
         // so also print the state.
