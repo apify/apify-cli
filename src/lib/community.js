20fd322981dfac9b5f6f3ed5d66d1da70f1f7a58
HonzaTuron <jan.turon@apify.com>
2023-05-23 09:22:45 +0200

    feat: add community message

---
+++ src/lib/community.js
@@ -0,0 +1,3 @@
+console.log('We have an active developer community on Discord. '
+            + 'You can find it on https://discord.gg/crawlee-apify-801163717915574323 or when you write apify -h.',
+);
