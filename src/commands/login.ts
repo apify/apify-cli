129b5d7405d268d482578370ca520282769f1da4
Jan Turoň <jan.turon@apify.com>
2024-03-12 09:51:04 +0100

    feat: use login-new as default login method (#508)
    Closes apify/apify-web#3440
    
    Login command migrated to use interactive mode as well. Added test to
    check if CLI listens correctly for token.
    
    ---------
    
    Co-authored-by: Vlad Frangu <me@vladfrangu.dev>

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -1,13 +1,42 @@
+import { Server } from 'http';
+import { AddressInfo } from 'net';
+
+import { cryptoRandomObjectId } from '@apify/utilities';
 import { Flags } from '@oclif/core';
+import computerName from 'computer-name';
+import cors from 'cors';
+import express from 'express';
 import inquirer from 'inquirer';
+import open from 'open';
 
 import { ApifyCommand } from '../lib/apify_command.js';
-import { error, success } from '../lib/outputs.js';
+import { error, info, success } from '../lib/outputs.js';
 import { useApifyIdentity } from '../lib/telemetry.js';
 import { getLocalUserInfo, getLoggedClient } from '../lib/utils.js';
 
+const CONSOLE_BASE_URL = 'https://console.apify.com/account?tab=integrations';
+// const CONSOLE_BASE_URL = 'http://localhost:3000/account?tab=integrations';
+const CONSOLE_URL_ORIGIN = new URL(CONSOLE_BASE_URL).origin;
+
+const API_BASE_URL = CONSOLE_BASE_URL.includes('localhost') ? 'http://localhost:3333' : undefined;
+
+// Not really checked right now, but it might come useful if we ever need to do some breaking changes
+const API_VERSION = 'v1';
+
+const tryToLogin = async (token: string) => {
+    const isUserLogged = await getLoggedClient(token, API_BASE_URL);
+    const userInfo = await getLocalUserInfo();
+    if (isUserLogged) {
+        await useApifyIdentity(userInfo.id!);
+        success(`You are logged in to Apify as ${userInfo.username || userInfo.id}!`);
+    } else {
+        error('Login to Apify failed, the provided API token is not valid.');
+    }
+    return isUserLogged;
+};
+
 export class LoginCommand extends ApifyCommand<typeof LoginCommand> {
-    static override description = 'Logs in to your Apify account using a provided API token.\nThe API token and other account '
+    static override description = 'Logs in to your Apify account.\nThe API token and other account '
     + 'information is stored in the ~/.apify directory, from where it is read by all other "apify" commands. '
     + 'To log out, call "apify logout".';
 
@@ -17,22 +46,137 @@ export class LoginCommand extends ApifyCommand<typeof LoginCommand> {
             description: '[Optional] Apify API token',
             required: false,
         }),
+        method: Flags.string({
+            char: 'm',
+            description: '[Optional] Method of logging in to Apify',
+            options: ['console', 'manual'],
+            required: false,
+        }),
     };
 
     async run() {
-        let { token } = this.flags;
-
-        if (!token) {
-            console.log('Enter your Apify API token. You can find it at https://console.apify.com/account#/integrations');
-            const tokenPrompt = await inquirer.prompt<{ token: string }>([{ name: 'token', message: 'token:', type: 'password' }]);
-            ({ token } = tokenPrompt);
+        const { token, method } = this.flags;
+        if (token) {
+            await tryToLogin(token);
+            return;
         }
 
-        const isUserLogged = await getLoggedClient(token);
-        const userInfo = await getLocalUserInfo();
-        await useApifyIdentity(userInfo.id!);
-        return isUserLogged
-            ? success(`You are logged in to Apify as ${userInfo.username || userInfo.id}!`)
-            : error('Login to Apify failed, the provided API token is not valid.');
+        let selectedMethod = method;
+
+        if (!method) {
+            const answer = await inquirer.prompt([{
+                type: 'list',
+                name: 'loginMethod',
+                message: 'Choose how you want to log in to Apify',
+                choices: [
+                    {
+                        value: 'console',
+                        name: 'Through Apify Console in your default browser',
+                        short: 'Through Apify Console',
+                    },
+                    { value: 'manual', name: 'Enter API token manually', short: 'Manually' },
+                ],
+                loop: true,
+            }]);
+
+            selectedMethod = answer.loginMethod;
+        }
+
+        if (selectedMethod === 'console') {
+            let server: Server;
+            const app = express();
+
+            // To send requests from browser to localhost, CORS has to be configured properly
+            app.use(cors({
+                origin: CONSOLE_URL_ORIGIN,
+                allowedHeaders: ['Content-Type', 'Authorization'],
+            }));
+
+            // Turn off keepalive, otherwise closing the server when command is finished is lagging
+            app.use((_, res, next) => {
+                res.set('Connection', 'close');
+                next();
+            });
+
+            app.use(express.json());
+
+            // Basic authorization via a random token, which is passed to the Apify Console,
+            // and that sends it back via the `token` query param, or `Authorization` header
+            const authToken = cryptoRandomObjectId();
+            app.use((req, res, next) => {
+                let { token: serverToken } = req.query;
+                if (!serverToken) {
+                    const authorizationHeader = req.get('Authorization');
+                    if (authorizationHeader) {
+                        const [schema, tokenFromHeader, ...extra] = authorizationHeader.trim().split(/\s+/);
+                        if (schema.toLowerCase() === 'bearer' && tokenFromHeader && extra.length === 0) {
+                            serverToken = tokenFromHeader;
+                        }
+                    }
+                }
+
+                if (serverToken !== authToken) {
+                    res.status(401);
+                    res.send('Authorization failed');
+                } else {
+                    next();
+                }
+            });
+
+            const apiRouter = express.Router();
+            app.use(`/api/${API_VERSION}`, apiRouter);
+
+            apiRouter.post('/login-token', async (req, res) => {
+                try {
+                    if (req.body.apiToken) {
+                        await tryToLogin(req.body.apiToken);
+                    } else {
+                        throw new Error('Request did not contain API token');
+                    }
+                    res.end();
+                } catch (err) {
+                    const errorMessage = `Login to Apify failed with error: ${(err as Error).message}`;
+                    error(errorMessage);
+                    res.status(500);
+                    res.send(errorMessage);
+                }
+                server.close();
+            });
+
+            apiRouter.post('/exit', (req, res) => {
+                if (req.body.isWindowClosed) {
+                    error('Login to Apify failed, the console window was closed.');
+                } else if (req.body.actionCanceled) {
+                    error('Login to Apify failed, the action was canceled in the Apify Console.');
+                } else {
+                    error('Login to Apify failed.');
+                }
+
+                res.end();
+                server.close();
+            });
+
+            // Listening on port 0 will assign a random available port
+            server = app.listen(0);
+            const { port } = server.address() as AddressInfo;
+
+            const consoleUrl = new URL(CONSOLE_BASE_URL);
+            consoleUrl.searchParams.set('localCliCommand', 'login');
+            consoleUrl.searchParams.set('localCliPort', `${port}`);
+            consoleUrl.searchParams.set('localCliToken', authToken);
+            consoleUrl.searchParams.set('localCliApiVersion', API_VERSION);
+            try {
+                consoleUrl.searchParams.set('localCliComputerName', encodeURIComponent(computerName()));
+            } catch {
+                // Ignore errors from fetching computer name as it's not critical
+            }
+
+            info(`Opening Apify Console at "${consoleUrl.href}"...`);
+            await open(consoleUrl.href);
+        } else {
+            console.log('Enter your Apify API token. You can find it at https://console.apify.com/account#/integrations');
+            const tokenAnswer = await inquirer.prompt<{ token: string }>([{ name: 'token', message: 'token:', type: 'password' }]);
+            await tryToLogin(tokenAnswer.token);
+        }
     }
 }
--- test/commands/log_in_out.test.ts
+++ test/commands/log_in_out.test.ts
@@ -1,16 +1,22 @@
 import { existsSync } from 'node:fs';
 
+import axios from 'axios';
 import { loadJsonFileSync } from 'load-json-file';
 import _ from 'underscore';
 
-import { LoginCommand } from '../../src/commands/login.js';
-import { LogoutCommand } from '../../src/commands/logout.js';
 import { AUTH_FILE_PATH } from '../../src/lib/consts.js';
 import { TEST_USER_BAD_TOKEN, TEST_USER_TOKEN, testUserClient } from '../__setup__/config.js';
 import { useAuthSetup } from '../__setup__/hooks/useAuthSetup.js';
 
 vitest.setConfig({ restoreMocks: false });
 useAuthSetup();
+
+vitest.mock('open', () => ({
+    default: (url: string) => console.log(`Open URL: ${url}`),
+}));
+
+const { LoginCommand } = await import('../../src/commands/login.js');
+const { LogoutCommand } = await import('../../src/commands/logout.js');
 
 describe('apify login and logout', () => {
     let spy: import('vitest').MockInstance<Parameters<typeof console['log']>, void>;
@@ -42,5 +48,32 @@ describe('apify login and logout', () => {
         const isGlobalConfig = existsSync(AUTH_FILE_PATH());
 
         expect(isGlobalConfig).to.be.eql(false);
+    });
+
+    it('have correctly setup server for interactive login', async () => {
+        await LoginCommand.run(['-m', 'console'], import.meta.url);
+
+        const consoleInfo = spy.mock.calls[0][0];
+        const consoleUrl = /"(http[s]?:\/\/[^"]*)"/.exec(consoleInfo)?.[1];
+
+        const consoleUrlParams = new URL(consoleUrl!).searchParams;
+
+        const localCliPort = consoleUrlParams.get('localCliPort');
+        const localCliToken = consoleUrlParams.get('localCliToken');
+
+        const response = await axios.post(`http://localhost:${localCliPort}/api/v1/login-token?token=${localCliToken}`,
+            { apiToken: TEST_USER_TOKEN },
+            { headers: { 'Content-Type': 'application/json' } },
+        );
+
+        expect(response.status).to.be.eql(200);
+
+        const expectedUserInfo = Object.assign(await testUserClient.user('me').get(), { token: TEST_USER_TOKEN });
+        const userInfoFromConfig = loadJsonFileSync(AUTH_FILE_PATH());
+
+        expect(spy.mock.calls[2][0]).to.include('Success:');
+        // Omit currentBillingPeriod, It can change during tests
+        const floatFields = ['currentBillingPeriod', 'plan', 'createdAt'];
+        expect(_.omit(expectedUserInfo, floatFields)).to.eql(_.omit(userInfoFromConfig, floatFields));
     });
 });
