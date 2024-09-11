feaf40d74362607f008e2d0ba8d1182058e73055
Vlad Frangu <me@vladfrangu.dev>
2024-09-11 14:47:45 +0300

    feat: `runs resurrect`

---
+++ src/commands/runs/resurrect.ts
@@ -0,0 +1,66 @@
+import { ACTOR_JOB_STATUSES } from '@apify/consts';
+import { Args } from '@oclif/core';
+import type { ApifyApiError } from 'apify-client';
+
+import { ApifyCommand } from '../../lib/apify_command.js';
+import { error, success } from '../../lib/outputs.js';
+import { getLoggedClientOrThrow } from '../../lib/utils.js';
+
+const resurrectStatuses = [
+	ACTOR_JOB_STATUSES.SUCCEEDED,
+	ACTOR_JOB_STATUSES.FAILED,
+	ACTOR_JOB_STATUSES.ABORTED,
+	ACTOR_JOB_STATUSES.TIMED_OUT,
+];
+
+export class RunsResurrectCommand extends ApifyCommand<typeof RunsResurrectCommand> {
+	static override description = 'Resurrects an aborted or finished Actor Run.';
+
+	static override args = {
+		runId: Args.string({
+			required: true,
+			description: 'The run ID to resurrect.',
+		}),
+	};
+
+	static override enableJsonFlag = true;
+
+	async run() {
+		const { runId } = this.args;
+
+		const apifyClient = await getLoggedClientOrThrow();
+
+		const run = await apifyClient.run(runId).get();
+
+		if (!run) {
+			error({ message: `Run with ID "${runId}" was not found on your account.` });
+			return;
+		}
+
+		if (!resurrectStatuses.includes(run.status as never)) {
+			error({
+				message: `Run with ID "${runId}" cannot be resurrected, as it is still running or in the process of aborting.`,
+			});
+
+			return;
+		}
+
+		try {
+			const result = await apifyClient.run(runId).resurrect();
+
+			if (this.flags.json) {
+				return result;
+			}
+
+			success({ message: `Run with ID "${runId}" was resurrected successfully.` });
+		} catch (err) {
+			const casted = err as ApifyApiError;
+
+			error({
+				message: `Failed to resurrect run "${runId}".\n  ${casted.message || casted}`,
+			});
+		}
+
+		return undefined;
+	}
+}
