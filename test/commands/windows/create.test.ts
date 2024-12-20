6106491d377afabd1d351af1e76ba13c3260cc63
Vlad Frangu <me@vladfrangu.dev>
2024-12-20 09:17:55 +0200

    chore: test correctly

--- test/commands/windows/create.test.ts
+++ test/commands/windows/create.test.ts
@@ -2,17 +2,17 @@ import { existsSync } from 'node:fs';
 
 import { useTempPath } from '../../__setup__/hooks/useTempPath.js';
 
-const actName = 'create my actor';
-const { beforeAllCalls, afterAllCalls, tmpPath } = useTempPath(actName, {
+const actName = 'create-my-spaced-actor';
+const { beforeAllCalls, afterAllCalls, joinPath } = useTempPath('spaced actor', {
 	create: true,
 	remove: true,
 	cwd: true,
-	cwdParent: true,
+	cwdParent: false,
 });
 
 const { CreateCommand } = await import('../../../src/commands/create.js');
 
-describe.runIf(process.platform === 'win32')('apify create on windows', () => {
+describe.runIf(process.env.FORCE_WINDOWS_TESTS || process.platform === 'win32')('apify create on windows', () => {
 	beforeEach(async () => {
 		await beforeAllCalls();
 	});
@@ -26,6 +26,6 @@ describe.runIf(process.platform === 'win32')('apify create on windows', () => {
 		await CreateCommand.run([actName, '--template', ACT_TEMPLATE], import.meta.url);
 
 		// check files structure
-		expect(existsSync(tmpPath)).toBeTruthy();
+		expect(existsSync(joinPath(actName))).toBeTruthy();
 	});
 });
