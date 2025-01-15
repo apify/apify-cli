fea2612dc671a530b440f7d72ce96ffe4a700b02
Michał Olender <michal.j.olender@gmail.com>
2025-01-15 10:21:06 +0100

    remove tabs & adjust secret description

--- src/commands/actor/push-data.ts
+++ src/commands/actor/push-data.ts
@@ -7,12 +7,12 @@ import { error } from '../../lib/outputs.js';
 
 export class PushDataCommand extends ApifyCommand<typeof PushDataCommand> {
 	static override description =
-	'Saves data to Actor\'s run default dataset.\n\n' +
-	'Accept input as:\n' +
-	'  - JSON argument:\n' +
-	'  $ apify actor push-data {"key": "value"}\n' +
-	'  - Piped stdin:\n' +
-	'  $ cat ./test.json | apify actor push-data\n';
+		'Saves data to Actor\'s run default dataset.\n\n' +
+		'Accept input as:\n' +
+		'  - JSON argument:\n' +
+		'  $ apify actor push-data {"key": "value"}\n' +
+		'  - Piped stdin:\n' +
+		'  $ cat ./test.json | apify actor push-data\n';
 
 	static override args = {
 		item: Args.string({
--- src/commands/secrets/index.ts
+++ src/commands/secrets/index.ts
@@ -3,19 +3,19 @@ import { LOCAL_CONFIG_PATH } from '../../lib/consts.js';
 
 export class SecretsIndexCommand extends ApifyCommand<typeof SecretsIndexCommand> {
 	static override description =
-	'  Manages secure environment variables for Actors.\n\n' +
-	'  Example:\n' +
-	'  $ apify secrets add mySecret TopSecretValue123\n\n' +
-	'  Now the "mySecret" value can be used in an environment variable defined in ".actor/actor.json" file by adding the "@"\n' +
-	'  prefix:\n\n' +
-	'  {\n' +
-	'    "actorSpecification": 1,\n' +
-	'    "name": "my_actor",\n' +
-	'    "environmentVariables": { "SECRET_ENV_VAR": "@mySecret" },\n' +
-	'    "version": "0.1"\n' +
-	'  }\n\n' +
-	'  When the Actor is pushed to Apify cloud, the "SECRET_ENV_VAR" and its value is stored as a secret environment variable\n' +
-	'  of the Actor.\n';
+	'Manages secure environment variables for Actors.\n\n' +
+	'Example:\n' +
+	'$ apify secrets add mySecret TopSecretValue123\n\n' +
+	'The "mySecret" value can be used in an environment variable defined in ".actor/actor.json" file by adding the "@"\n' +
+	'prefix:\n\n' +
+	'{\n' +
+	'  "actorSpecification": 1,\n' +
+	'  "name": "my_actor",\n' +
+	'  "environmentVariables": { "SECRET_ENV_VAR": "@mySecret" },\n' +
+	'  "version": "0.1"\n' +
+	'}\n\n' +
+	'When the Actor is pushed to Apify cloud, the "SECRET_ENV_VAR" and its value is stored as a secret environment variable\n' +
+	'of the Actor.\n';
 
 	async run() {
 		await this.printHelp();
