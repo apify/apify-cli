f28822623bafc5cf620b04ebec30a2c922f33d0d
Jan Buchar <Teyras@gmail.com>
2025-02-24 13:10:54 +0100

    ci: Fix pre-release

--- .github/scripts/before-beta-release.ts
+++ .github/scripts/before-beta-release.ts
@@ -1,9 +1,10 @@
-import { execSync } from 'node:child_process';
-import { readFile, writeFile } from 'node:fs/promises';
+import path from 'path';
+import { readFile, writeFile } from 'fs/promises';
+import { execSync } from 'child_process';
 
-const PKG_JSON_PATH = new URL('../../package.json', import.meta.url);
+const PKG_JSON_PATH = path.join(__dirname, '..', '..', 'package.json');
 
-const pkgJson = JSON.parse(await readFile(PKG_JSON_PATH, 'utf8'));
+const pkgJson = JSON.parse(await readFile(PKG_JSON_PATH, { encoding: 'utf8' }));
 
 const PACKAGE_NAME = pkgJson.name;
 const VERSION = pkgJson.version;
