0a4be057425a27380794d9e3504f4a657fd41e92
Vlad Frangu <me@vladfrangu.dev>
2025-07-11 19:41:28 +0300

    chore: bundle the CLI in one file for npm users

--- src/entrypoints/apify.ts
+++ src/entrypoints/apify.ts
@@ -1,18 +1,8 @@
 #!/usr/bin/env node
-import { satisfies } from 'semver';
-
 import { apifyCommands } from '../commands/_register.js';
-import { SUPPORTED_NODEJS_VERSION } from '../lib/consts.js';
-import { error } from '../lib/outputs.js';
-import { cli, runCLI } from './_shared.js';
+import { cli, processVersionCheck, runCLI } from './_shared.js';
 
-if (!satisfies(process.version, SUPPORTED_NODEJS_VERSION)) {
-	error({
-		message: `Apify CLI requires Node.js version ${SUPPORTED_NODEJS_VERSION}. Your current version is ${process.version}.`,
-	});
-
-	process.exit(1);
-}
+processVersionCheck('Apify');
 
 // Register all commands
 for (const CommandClass of apifyCommands) {
