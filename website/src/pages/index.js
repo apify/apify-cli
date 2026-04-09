8dfc5c699d96c82a9782bd57b32c0160eedc08e3
Vlad Frangu <me@vladfrangu.dev>
2026-04-09 12:23:09 +0300

    fix: website and eslint

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
 import { useHistory } from '@docusaurus/router';
+import { useEffect } from 'react';
 
 export default function Home() {
     const history = useHistory();
